fc556f612bebae81b31b0c792ce994e36cece831
Reis <aglemar.reis@gmail.com>
2024-05-16 18:22:02 -0300

    feat: novos arquivos

---
+++ ZonaDeTestes/src/main/java/edu/reis/zonadetestes/finall/ZonaDeTestes.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package edu.reis.zonadetestes.finall;
+
+/**
+ *
+ * @author reis
+ */
+public class ZonaDeTestes {
+
+    public static void main(String[] args) {
+
+        final int VALOR = 10;
+
+        String saida = (VALOR >= 10)
+                ? "maior"
+                : "menor";
+
+        System.out.println(saida);
+    }
+}
