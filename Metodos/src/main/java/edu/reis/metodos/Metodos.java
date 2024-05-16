47de5df5c8037c1099fe15abd1dde309f60cb560
Reis <aglemar.reis@gmail.com>
2024-05-16 11:37:07 -0300

    fix: criado arquivo de anotações e classe Metodos

---
+++ Metodos/src/main/java/edu/reis/metodos/Metodos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.reis.metodos;
+
+/**
+ *
+ * @author reis
+ */
+public class Metodos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
