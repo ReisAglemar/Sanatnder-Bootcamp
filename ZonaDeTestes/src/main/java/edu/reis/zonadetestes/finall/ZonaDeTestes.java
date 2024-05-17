fa3b88a1fc95a196c3e6364b4b967622e41faee4
Reis <aglemar.reis@gmail.com>
2024-05-17 13:04:40 -0300

    feat: testado novos algoritimos

--- ZonaDeTestes/src/main/java/edu/reis/zonadetestes/finall/ZonaDeTestes.java
+++ ZonaDeTestes/src/main/java/edu/reis/zonadetestes/finall/ZonaDeTestes.java
@@ -11,12 +11,76 @@ public class ZonaDeTestes {
 
     public static void main(String[] args) {
 
-        final int VALOR = 10;
+//        String plano = "T"; // M / T
+//        switch (plano) {
+//            case "T": {
+//                System.out.println("5Gb Youtube");
+//            }
+//            case "M": {
+//                System.out.println("Whats e Intagram grátis");
+//
+//            }
+//            case "B": {
+//                System.out.println("100 minutos de ligação");
+//            }
+//        }
+//
+//        int teste = 10;
+//
+//        for (int i = 0; i < teste; i++) {
+//            System.out.println(i + " apenas i");
+//            System.out.println(++i + " incrementa é imprime será um a mais que aciima");
+//            System.out.println(i++ + "será igual acima depois mais um");
+//            System.out.println(i + " ");
+//
+//        }
+//        boolean teste = false;
+//
+//        String reposta = (teste) ? "true" : "false";
+//        System.out.println(reposta);
+//        for (int x = 0; x < 10; x++) {
+//            if (x == 6) continue;
+//            if (x == 8) break;
+//            switch (x) {
+//            case 0:
+//                System.out.println("zero");
+//                continue;
+////                  break;
+//            case 1:
+//                System.out.println("um");
+////                continue;
+//                  break;
+//            case 2:
+//                System.out.println("dois");
+//                break;
+//            default:
+//                System.out.println(x);
+//                break;
+//            }
+//        }
+//
+//        int numero = 100;
+//
+//        for (int i = 0; i < numero; i++) {
+//
+//            boolean dividePor3 = (i % 3 == 0);
+//            boolean dividepor5 = (i % 5 == 0);
+//
+//            if (dividePor3 && dividepor5) {
+//                System.out.println("FIZZ BUZZ");
+//                continue;
+//            }
+//            if (dividePor3) {
+//                System.out.println("FIZZ");
+//                continue;
+//            }
+//            if (dividepor5) {
+//                System.out.println("BUZZ");
+//                continue;
+//            }
+//            System.out.println(i);
+//
+//        }
 
-        String saida = (VALOR >= 10)
-                ? "maior"
-                : "menor";
-
-        System.out.println(saida);
     }
 }
